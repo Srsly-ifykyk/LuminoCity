13a76f4069e62a65438646736af614f98dcf78cd
Srsly-ifykyk <3animmanu7@gmail.com>
2026-03-12 06:41:27 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,11 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/LuminoCity/',
-})
-
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    base: '/LuminoCity/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
